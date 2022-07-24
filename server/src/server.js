99f54a211bac5e1892f0617029605572d76c3600
Ghost Order <kodaichi.kun@gmail.com>
2022-07-23 19:18:55 -0500

    first commit

---
+++ server/src/server.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const app = require('./app');
+const { loadHabitablePlanets } = require('./models/planets.model');
+
+const server = http.createServer(app);
+
+const PORT = process.env.PORT || 8000;
+
+async function startServer () {
+  await loadHabitablePlanets();
+  server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
+}
+
+startServer();
